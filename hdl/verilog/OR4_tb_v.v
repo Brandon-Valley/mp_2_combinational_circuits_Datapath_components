5968d79df6c9420999a671d5430093bcfeb43108
Brandon Valley <bavvh8@mst.edu>
2020-02-22 10:04:29 -0600

    OR4 cmpnt_self works

--- hdl/verilog/OR4_tb_v.v
+++ hdl/verilog/OR4_tb_v.v
@@ -5,7 +5,6 @@
 
 module OR4_tb_v;
   wire o_f;
-  // reg i_a, i_b;
   reg i_a, i_b, i_c, i_d;
   reg [1:4] d_in = 4'b0000;
   integer i;
@@ -14,26 +13,15 @@ module OR4_tb_v;
   //              OR4_v__behavior
   //              OR4_v__cmpnt_prim
   //              OR4_v__cmpnt_self
-  OR4_v__behavior duv (.i_a(i_a), .i_b(i_b), .i_c(i_c), .i_d(i_d), .o_f(o_f)); // port map
+  OR4_v__cmpnt_self duv (.i_a(i_a), .i_b(i_b), .i_c(i_c), .i_d(i_d), .o_f(o_f)); // port map
 
   //procedure statement
   initial begin
-            // i_a = 0; i_b = 0; i_c = 0; i_d = 0;
-      // #1000      i_a = 0; i_b = 0; i_c = 1; i_d = 0;
-      // #1000      i_a = 0; i_b = 1; i_c = 0; i_d = 0;
-      // #1000      i_a = 1; i_b = 0; i_c = 0; i_d = 0;
-      // // #1000 i_a = 0; i_b = 1;
-      // // #1000 i_a = 1; i_b = 0;
-      // // #1000 i_a = 1; i_b = 1;
-      
-      // #1000 $wait;
-      
       
       for (i = 0 ; i < 17 ; i = i + 1) begin
         #1000 i_a = d_in[1]; i_b = d_in[2]; i_c = d_in[3]; i_d = d_in[4];
-        d_in = d_in +1;
+        d_in = d_in + 1;
       end
-    
     
     end
 
--- hdl/verilog/OR4_v.v
+++ hdl/verilog/OR4_v.v
@@ -21,9 +21,67 @@ module OR4_v__behavior
    
   assign o_f = 1 ? i_a | i_b | i_c | i_d : 0;
   
+endmodule
+
+
+
+////////////////////////////////
+// Component Model - Self
+////////////////////////////////
+module OR4_v__cmpnt_self
+  (input i_a, i_b, i_c, i_d,
+  output o_f);
+   
+  wire fi1, fi2; // internal outputs
+   
+  OR2_v or1 (i_a, i_b, fi1);
+  OR2_v or2 (i_c, i_d, fi2);
+  OR2_v or3 (fi1, fi2, o_f);
   
+  // assign o_f = 1 ? i_a | i_b | i_c | i_d : 0;
   
 endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
