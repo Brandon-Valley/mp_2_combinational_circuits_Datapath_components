4d669729fbf4ef9f959def469ddd72ad2a69c295
Brandon Valley <bavvh8@mst.edu>
2020-02-23 07:16:30 -0600

    micro works

--- hdl/verilog/micro_tb_v.v
+++ hdl/verilog/micro_tb_v.v
@@ -2,20 +2,8 @@
 
 `timescale 1ms/1ms
 
-  // input        i_en,
-  // input  [3:0] i_code,
-  // output       o_A,
-  // output       o_L,
-  // output       o_B);
   
 module micro_tb_v;
-  
-  // reg        i_en;
-  // reg  [7:0] i_code_0;
-  // reg  [7:0] i_code_1;
-  // reg  [7:0] i_code_2;
-  // reg  [7:0] i_code_3;
-  
   
   reg        i_en;
   reg  [3:0] i_code;
@@ -24,44 +12,38 @@ module micro_tb_v;
   wire       o_B;
   
   
-  // reg  [1:0] i_sel_code;
-  // wire [7:0] o_code;
-  
-  
-  // reg [2:0] d_in = 1'b0;
-  // integer i;
-  
   // duv port map options:
   micro_v duv (.i_en(i_en), 
                .i_code(i_code),
                .o_A(o_A),
                .o_L(o_L),
                .o_B(o_B));
-  // micro_v__cmpnt duv (.i_en(i_en), .i_code(i_code), .i_sel_code(i_sel_code), .o_f(o_f));
   
   //procedure statement
   initial begin
 
+    #1000 i_code = 4'b0011; i_en = 0;
+    #1000 i_code = 4'b1010; i_en = 0;
+    #1000 i_code = 4'b1011; i_en = 0;
+    #1000 i_code = 4'b0100; i_en = 0;
+    #1000 i_code = 4'b1100; i_en = 0;
+    #1000 i_code = 4'b0010; i_en = 0;
+    #1000 i_code = 4'b1110; i_en = 0;
+    #1000 i_code = 4'b1111; i_en = 0;
+    
+    #1000 i_code = 4'b0011; i_en = 1;
+    #1000 i_code = 4'b1010; i_en = 1;
+    #1000 i_code = 4'b1011; i_en = 1;
+    #1000 i_code = 4'b0100; i_en = 1;
+    #1000 i_code = 4'b1100; i_en = 1;
+    #1000 i_code = 4'b0010; i_en = 1;
+    #1000 i_code = 4'b1110; i_en = 1;
+    #1000 i_code = 4'b1111; i_en = 1;
+    
     #1000 i_code = 4'b0011; i_en = 1;
 
-      
-      // // for (i = 0 ; i < 10 ; i = i + 1) begin
 
-        // #1000 i_sel_code[0] = d_in[0];
-              // i_sel_code[1] = d_in[1];
-              // i_en          = d_in[2];
-              
-              // i_code_0 = 8'b10000000;
-              // i_code_1 = 8'b01000000;
-              // i_code_2 = 8'b11000000;
-              // i_code_3 = 8'b00100000;
-        
-
-        // // i_sel_code[0] = d_in[1]; o_code = d_in[2]; i_d = d_in[3]; o_codes = d_in[4]; i_n_cs_0 = d_in[5]; ;
-        // d_in = d_in + 1;
-      // end
-          
-    end
+  end
 
 endmodule
 
--- hdl/verilog/micro_v.v
+++ hdl/verilog/micro_v.v
@@ -1,10 +1,5 @@
 // -- python C:\Users\Brandon\Documents\Personal_Projects\my_utils\modelsim_utils\auto_run.py -d run_cmd__micro_v.do
 
-// sim:/micro_tb_v/i_en \
-// sim:/micro_tb_v/i_code \
-// sim:/micro_tb_v/o_A \
-// sim:/micro_tb_v/o_L \
-// sim:/micro_tb_v/o_B 
 
 //  Behavior Model
 module micro_v
@@ -16,14 +11,19 @@ module micro_v
   output       o_B);
   
   
-  assign o_A = 1;
-  assign o_L = 1;
-  assign o_B = 1;
+  assign o_A = ~i_en ? 'Z:
+               i_code == 4'b1010 | 
+               i_code == 4'b1011 |
+               i_code == 4'b1100 ? 1 : 0;
   
-  // assign o_code = i_sel_code == 2'b00 & i_en ? i_code_0 :
-                  // i_sel_code == 2'b01 & i_en ? i_code_1 :
-                  // i_sel_code == 2'b10 & i_en ? i_code_2 :
-                  // i_sel_code == 2'b11 & i_en ? i_code_3 : 8'b00000000;
+  assign o_L = ~i_en ? 'Z:
+               i_code == 4'b0011 | 
+               i_code == 4'b0100 |
+               i_code == 4'b0010 |
+               i_code == 4'b1110 ? 1 : 0;
+               
+  assign o_B = ~i_en ? 'Z:
+               i_code == 4'b1111 ? 1 : 0;
   
 endmodule
 
