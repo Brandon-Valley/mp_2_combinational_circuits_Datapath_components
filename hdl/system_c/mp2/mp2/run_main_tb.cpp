dd8cf276dbd9b4a33f75715ef6e6717a7c3ea307
Brandon Valley <bavvh8@mst.edu>
2020-03-18 22:41:03 -0500

    SN7 test sim works

--- hdl/system_c/mp2/mp2/run_main_tb.cpp
+++ hdl/system_c/mp2/mp2/run_main_tb.cpp
@@ -5,6 +5,7 @@
 #include "xor_gate__tb.h"
 #include "priority_enc_4_2__tb.h"
 #include "decoder_2_4_active_low_out__tb.h"
+#include "SN74145__tb.h"
 
 
 int sc_main(int argc, char* argv[])
@@ -14,7 +15,8 @@ int sc_main(int argc, char* argv[])
     //xor4_gate__tb();
 
     //priority_enc_4_2__tb();
-    decoder_2_4_active_low_out__tb();
+    //decoder_2_4_active_low_out__tb();
+    SN74145__tb();
 
     return 0;
 }
