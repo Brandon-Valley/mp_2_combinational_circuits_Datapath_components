2cdb347b4fb93c697fc2abba967520b8b65270d9
Brandon Valley <bavvh8@mst.edu>
2020-03-20 00:01:47 -0500

    VVV test sim works

--- hdl/system_c/mp2/mp2/MUX_4_1.h
+++ hdl/system_c/mp2/mp2/MUX_4_1.h
@@ -10,29 +10,28 @@ SC_MODULE(MUX_4_1__behavior)
 {
     //  Define IO Ports
     sc_in  <bool>     i_en      ;
-    sc_in  <sc_lv<8>> i_code_0  ;
-    sc_in  <sc_lv<8>> i_code_1  ;
-    sc_in  <sc_lv<8>> i_code_2  ;
-    sc_in  <sc_lv<8>> i_code_3  ;
+    sc_in  <sc_lv<4>> i_code    ;
     sc_in  <sc_lv<2>> i_sel_code;
-    sc_out <sc_lv<8>> o_code    ;
-
-
+    sc_out <bool>     o_f       ;
 
 
     // Architecture Statement - Similar to Process Statement
     void p1()
     {
 
-        if (i_en)
-        {
-            if (i_sel_code.read() == "00")  o_code = i_code_0;
-            if (i_sel_code.read() == "01")  o_code = i_code_1;
-            if (i_sel_code.read() == "10")  o_code = i_code_2;
-            if (i_sel_code.read() == "11")  o_code = i_code_3;
-        }
-        else
-            o_code = "00000000";
+        //if (i_en)
+        //{
+        //    if (i_sel_code.read() == "00")  o_code = i_code_0;
+        //    if (i_sel_code.read() == "01")  o_code = i_code_1;
+        //    if (i_sel_code.read() == "10")  o_code = i_code_2;
+        //    if (i_sel_code.read() == "11")  o_code = i_code_3;
+        //}
+        //else
+        //    o_code = "00000000";
+
+        o_f.write(1);
+
+
     }
 
 
@@ -43,12 +42,8 @@ SC_MODULE(MUX_4_1__behavior)
 
         //  Input Sensitivity List
         sensitive << i_en      
-                  << i_code_0  
-                  << i_code_1  
-                  << i_code_2  
-                  << i_code_3  
+                  << i_code    
                   << i_sel_code
-                  << o_code    
                   ;
     }
 };
--- hdl/system_c/mp2/mp2/MUX_4_1__tb.h
+++ hdl/system_c/mp2/mp2/MUX_4_1__tb.h
@@ -25,12 +25,9 @@ void MUX_4_1__tb()
     //  Define IO Ports
     //=============================//
     sc_signal <bool>     i_en      ;
-    sc_signal <sc_lv<8>> i_code_0  ;
-    sc_signal <sc_lv<8>> i_code_1  ;
-    sc_signal <sc_lv<8>> i_code_2  ;
-    sc_signal <sc_lv<8>> i_code_3  ;
+    sc_signal <sc_lv<4>> i_code    ;
     sc_signal <sc_lv<2>> i_sel_code;
-    sc_signal <sc_lv<8>> o_code    ;
+    sc_signal <bool>     o_f       ;
 
 
     //=============================//
@@ -43,13 +40,10 @@ void MUX_4_1__tb()
     //  Port Map
     //=============================//
     DUT.i_en      (i_en      );
-    DUT.i_code_0  (i_code_0  );
-    DUT.i_code_1  (i_code_1  );
-    DUT.i_code_2  (i_code_2  );
-    DUT.i_code_3  (i_code_3  );
+    DUT.i_code    (i_code    );
     DUT.i_sel_code(i_sel_code);
-    DUT.o_code    (o_code    );
-    
+    DUT.o_f       (o_f       );
+
 
     // trace file to look at sim output
     sc_trace_file* fp1;  //create VCD file: file pointer fp1
@@ -60,12 +54,9 @@ void MUX_4_1__tb()
     //  Add Signals to Trace File
     //=============================//
     sc_trace(fp1, i_en      ,  "i_en");
-    sc_trace(fp1, i_code_0  ,  "i_code_0");
-    sc_trace(fp1, i_code_1  ,  "i_code_1");
-    sc_trace(fp1, i_code_2  ,  "i_code_2");
-    sc_trace(fp1, i_code_3  ,  "i_code_3");
+    sc_trace(fp1, i_code    ,  "i_code");
     sc_trace(fp1, i_sel_code,  "i_sel_code");
-    sc_trace(fp1, o_code    ,  "o_code");
+    sc_trace(fp1, o_f       ,  "o_f");
 
 
     //--------------//
@@ -74,33 +65,33 @@ void MUX_4_1__tb()
     print_sim_header("MUX_4_1");
     double num_combos_to_test = pow(num_bits_needed_in_sim_vec, 2) + 2; // run 2 extra so waveform always ends with all high, then all low
 
-    for (int i = 0; i < pow(3, 2) + 1; i++)
+    for (int i = 0; i < pow(4, 2) + 1; i++)
     {
         vector<int> sv = int_to_binary_vec__with_rollover(i, num_bits_needed_in_sim_vec); // simulation vector
         cout << "In MUX_4_1__tb.h, Sim:  i:" << i << "    sv:" << sv << endl;
-
-        //i_a = i % 2;
-
-        //i_sel_code = i - 1;
-
-        i_sel_code = i;
-        i_en = i % 2;
-
-        i_code_0 = i + 0;
-        i_code_1 = i + 1;
-        i_code_2 = i + 2;
-        i_code_3 = i + 3;
-
 
 
         //=============================//
         //  Set Inputs      
         //=============================//
-        //i_code[0] = sv[0];
-        //i_code = i;
-        //o_valid = sv[2];
-        //i_d = sv[3];
+        
+        i_sel_code = 0;
+        i_code = i;
 
+        sc_start(10, SC_NS); // run sim for 10 ns
+        
+        i_sel_code = 1;
+        i_code = i;
+
+        sc_start(10, SC_NS); // run sim for 10 ns
+        
+        i_sel_code = 2;
+        i_code = i;
+
+        sc_start(10, SC_NS); // run sim for 10 ns
+        
+        i_sel_code = 3;
+        i_code = i;
 
         sc_start(10, SC_NS); // run sim for 10 ns
     }
